dc41155034eea7c85300901f8ce049fe3d6ad97b
nathil <nathiellycastro282@gmail.com>
2025-03-15 20:16:00 -0300

    add

--- teste/questao03.py
+++ teste/questao03.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 caminho = 'arquivo2.csv'
 
 def retorna_lista(caminho): 
@@ -13,20 +15,40 @@ def retorna_lista(caminho):
     
     return registros
 
+
+
+# def quantidade_de_carga_por_tipo(lista_cargas, cod_setor):
+#     lista_cargas = retorna_lista(caminho)
+
+#     dicionario = {}
+
+#     for carga in lista_cargas: 
+#         if cod_setor == carga['Codigo'][:2]:
+#             if carga['Tipo'] in dicionario: 
+#                 dicionario[carga['Tipo']] += carga['Quantidade']
+#             else: 
+#                 dicionario[carga['Tipo']] = carga['Quantidade']
+        
+#     return dicionario
+
+
 def quantidade_de_carga_por_tipo(lista_cargas, cod_setor):
     lista_cargas = retorna_lista(caminho)
-
-    dicionario = {}
-
-    for carga in lista_cargas: 
-        if cod_setor == carga['Codigo'][:2]:
-            if carga['Tipo'] in dicionario: 
-                dicionario[carga['Tipo']] += carga['Quantidade']
+    
+    def funcao(dicionario, item):
+        if cod_setor == item['Codigo'][:2]:
+            if item['Tipo'] in dicionario: 
+                dicionario[item['Tipo']] += item['Quantidade']
             else: 
-                dicionario[carga['Tipo']] = carga['Quantidade']
+                dicionario[item['Tipo']] = item['Quantidade']
+        
+        return dicionario
+
+    dicionario = reduce(funcao, lista_cargas,{})
         
     return dicionario
 
 lista_cargas = retorna_lista(caminho)
 
 print(quantidade_de_carga_por_tipo(lista_cargas,'AO'))
+
